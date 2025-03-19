e40114ce899fc260ead1524d68887bb8a1d644e1
tiantiancheng <tiantiancheng@baidu.com>
2025-03-19 20:27:23 +0800

    feat: 增加线上支持

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as fs from 'fs/promises';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('clustering-data')
+  async getClusteringData() {
+    // 在服务器端读取 /home/data/clustering_data.csv
+    const data = await fs.readFile('/home/data/clustering_data.csv', 'utf-8');
+    return data;
+  }
+
+  @Get('steady-state-data')
+  async getSteadyStateData() {
+    // 在服务器端读取 /home/data/steady_state_data.csv
+    const data = await fs.readFile('/home/data/steady_state_data.csv', 'utf-8');
+    return data;
   }
 }
